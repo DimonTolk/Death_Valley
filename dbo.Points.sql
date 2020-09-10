676a7ccd4520b151ad43995645c9a2bb7fd1aa5a
DimonTolk <49361768+DimonTolk@users.noreply.github.com>
2020-09-10 20:07:28 +0300

    Add files via upload

---
+++ dbo.Points.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Points]
+(
+	[PointId] INT NOT NULL IDENTITY,
+	[ChartID] INT NOT NULL,
+	[PointX] INT NOT NULL,
+	[Pointy] INT NOT NULL,
+	PRIMARY KEY ([PointId] ASC),
+	FOREIGN KEY ([ChartId]) REFERENCES [dbo].[UserData] ([UserDataId]) ON DELETE CASCADE
+)
---
+++ dbo.UserDatas.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[UserDatas] (
+    [UserDataId] INT        IDENTITY (1, 1) NOT NULL,
+    [a]          INT        NOT NULL,
+    [b]          INT        NOT NULL,
+    [c]          INT        NOT NULL,
+    [step]       FLOAT (53) NOT NULL,
+    [RangeFrom]  INT        NOT NULL,
+    [RangeTo]    INT        NOT NULL,
+    PRIMARY KEY CLUSTERED ([UserDataId] ASC)
+);
+
